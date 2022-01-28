86ce045def7db5264a89308c1e5bc83866220641
belgica-real <rzomber123@gmail.com>
2022-01-28 14:22:11 -0300

    pasaje del autor del mensaje al socket

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, ".env") });
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
-const CHANNEL_ID = '927306095335211049';
+const CHANNEL_ID = '600102752059654144';
 
 
 client.on("ready", () => {
@@ -20,9 +20,10 @@ wss.on("connection", (ws) => {
 
         if (message.channel.id !== CHANNEL_ID) return;
 
+        const messageBy = message.member.nickname ?? message.author.username;
         const messageString = message.content.toString();
 
-        if (messageString.charAt(0) === "!") ws.send(messageString);
+        if (messageString.charAt(0) === "!") ws.send(JSON.stringify({ author: messageBy + " [📡]", message: messageString, createdAt: message.createdAt }));
     });
 
     ws.on("message", (msg) => {
